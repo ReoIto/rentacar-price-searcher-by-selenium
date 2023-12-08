1cc23f4feb1237e247c3166fbfffc0a700adece9
Reo Ito <leo.0210.ito@gmail.com>
2023-12-09 02:06:26 +0900

    paramsのkey名をスネークケースにする処理をApplicatonControllerに移譲

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :params_to_snake_case!
+
+  private
+
+    def params_to_snake_case!
+      params.transform_keys! { |k| k.underscore }
+    end
 end
--- app/controllers/search_prices_controller.rb
+++ app/controllers/search_prices_controller.rb
@@ -3,21 +3,17 @@ class SearchPricesController < ApplicationController
     result = CarPriceSearcher.execute(search_params[:start_date], search_params[:start_time], search_params[:return_date], search_params[:return_time])
 
     if result.success?
-      render json: {search_result: result.data}, status: :ok
+      render json: { search_result: result.data }, status: :ok
     else
-      render json: {
-        search_result: {
-          is_error: true
-        }
-      }, status: :internal_server_error
+      render json: { search_result: { is_error: true }}, status: :internal_server_error
     end
   end
 
   private
 
-  def search_params
-    return @search_params if defined? @search_params
+    def search_params
+      return @search_params if defined? @search_params
 
-    @search_params = params.transform_keys! { |k| k.underscore }.permit(:start_date, :start_time, :return_date, :return_time)
-  end
+      @search_params = params.permit(:start_date, :start_time, :return_date, :return_time)
+    end
 end
